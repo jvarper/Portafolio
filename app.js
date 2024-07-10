07d2049e0fd8f68ace65d62cce6399518a2e866e
JuanVargasCRM <dvargas@clicoh.com>
2024-07-09 21:29:11 -0500

    CreateRepo

---
+++ app.js
@@ -0,0 +1,9 @@
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const subject = document.getElementById("asunto");
+const text = document.getElementById("parrafo");
+
+function sendFormContact () {
+    alert(`Hola ${nombre}`);
+    
+}
